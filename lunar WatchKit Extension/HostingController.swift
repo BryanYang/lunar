92e5664de4fd8420944882bbf441ec962b0a9fe4
杨高飞 <yanggaofei@meituan.com>
2019-08-11 23:10:47 +0800

    Initial Commit

---
+++ lunar WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  lunar WatchKit Extension
+//
+//  Created by Bryan Yang on 2019/8/11.
+//  Copyright © 2019 Bryan Yang. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ lunar WatchKit Extension/NotificationView.swift
@@ -0,0 +1,23 @@
+//
+//  NotificationView.swift
+//  lunar WatchKit Extension
+//
+//  Created by Bryan Yang on 2019/8/11.
+//  Copyright © 2019 Bryan Yang. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
+#endif
